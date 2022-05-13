cecfdfa7cedf53147d3f484829c42b8d2a7f0633
noric <info@noricgepgraphic.com>
2022-05-13 10:27:02 +0900

    initialize

---
+++ src/main/java/com/example/sampleapp/SampleAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.sampleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class SampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleAppApplication.class, args);
+	}
+}
